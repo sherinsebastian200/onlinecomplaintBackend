0de9bff01498b382af669195bd7525f6fdc0fbbb
sherin200 <sherinsebastian21>
2022-12-11 00:16:58 +0530

    first

---
+++ src/main/java/com/example/onlinecomplaintbackend/OnlinecomplaintbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinecomplaintbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinecomplaintbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinecomplaintbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlinecomplaintbackend/OnlinecomplaintbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinecomplaintbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinecomplaintbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
